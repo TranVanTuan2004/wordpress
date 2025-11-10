8d6fc62297c72f0ab96695ebc2314f7d52190464
Tran Van Tuan (Kai) <tuantran280504@gmail.com>
2025-11-10 08:27:03 +0700

    config port

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,9 @@ define( 'DB_CHARSET', 'utf8mb4' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define('WP_HOME', 'http://localhost:8000');
+define('WP_SITEURL', 'http://localhost:8000');
+
 /**#@+
  * Authentication unique keys and salts.
  *
